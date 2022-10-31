a172dde30dd2dd7098b5f4bcadeda05a552cc59e
dmcbdh31 <dmcbdh@gmail.com>
2022-10-31 17:34:26 -0400

    Hero-Enemy update 1

--- Hero_Enemy.py
+++ Hero_Enemy.py
@@ -1,17 +1,18 @@
-from asyncio import start_server
-from decimal import ConversionSyntax
-from email.headerregistry import ContentDispositionHeader
-from opcode import stack_effect
-from sys import builtin_module_names
-from xmlrpc.client import Boolean
+#from asyncio import start_server
+#from decimal import ConversionSyntax
+#from email.headerregistry import ContentDispositionHeader
+#from opcode import stack_effect
+#from sys import builtin_module_names
+#from xmlrpc.client import Boolean
 
+import random
 
 Hero = {
     'name': 'Teenage Mutant Ninja Turtles',
     'level': 1,
     'health': 100,
     'equipment': 'Nunchucks, Sai, Katana, Bo Staff',
-    'attacks': 'power kicks, power punches, Equipment',
+    'attacks': (('power kicks, 200), (power punches, 100), ([equipment], 500)')),
 
     'Coins': {
         'Copper': 300,
@@ -26,16 +27,15 @@ Enemy_one = [
     'Level': 1,
     'Health': 100,
     'Equipment': 'Hanbo, Tonfa, Tanbo, Sansetsuko',
-    'attacks': 'kicks, punches, Equipment',
+    'attacks': (('kicks, 20), (punches, 10), ([Equipment], 50')),
 
     'Coins': {
         'Copper': 300,
         'Silver': 200,
-        'Gold': 100,
+        'Gold': 100
             }
 }
 ]
-    
 
 Enemy_two = [
 {
@@ -43,30 +43,40 @@ Enemy_two = [
     'Level': 2,
     'Health': 100,
     'Equipment': 'Kama, Eku, Tekko, Nunti Bo',
-    'attacks': 'kicks, punches, Equipment',
+    'attacks': (('kicks, 20) (punches, 10), ([Equipment], 50')),
 
     'Coins': {
         'Copper': 300,
         'Silver': 200,
-        'Gold': 100,
+        'Gold': 100
             }
-}            
+}
 ]
-    
 
 Enemy_three = [
 {
     'Name': 'Foot Clan',
     'Level': 3,
     'Equipment': 'Ninja Stars, Katana, Smoke Grenades',
-    'attacks': 'kicks, punches, Equipment',
+    'attacks': (('kicks, 20), (punches, 10) ([Equipment], 50')),
 
     'Coins': {
         'Copper': 300,
         'Silver': 200,
         'Gold': 100,
-            }
+            } 
 }
-]    
+]
 
-print(Hero['name'])
+enemy_list = {
+    "Enemy_one" : Enemy_one,
+    "Enemy_two" : Enemy_two,
+    "Enemy_three" : Enemy_three
+}
+
+random_hero_attack = random.shuffle("Hero['attacks]")
+random_enemy_attack = random.shuffle("Enemy_one"['attacks'],
+    "Enemy_two"['attacks'], "Enemy_three"['attacks'])
+
+
+
