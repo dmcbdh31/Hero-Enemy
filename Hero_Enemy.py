2e4870f7df7b4691b4f972e8c9d5b2c6b3dfcf4d
dmcbdh31 <dmcbdh@gmail.com>
2022-11-04 15:19:57 -0400

    Updated Game and Hero_Enemy 2

--- Hero_Enemy.py
+++ Hero_Enemy.py
@@ -3,9 +3,9 @@ Hero = {
     'level': 1,
     'health': 100,
     'equipment': 'Nunchucks, Sai, Katana, Bo Staff',
-    'attacks': 
-        (("power kicks", '25'), ("power punches", '15'), ("Nunchucks", '50'), ("Sai", '50')
-        ("Katana", '50'), ("Bo Staff", '50')),
+    'attacks': [ 
+            (('power kicks', 25), ('power punches', 15), ('Nunchucks', 50), 
+        ('Sai', 50), ('Katana', 50), ('Bo Staff', 50))],
 
     'Coins': {
         'Copper': 300,
@@ -19,8 +19,9 @@ Enemy_one = {
     'Level': 1,
     'Health': 100,
     'Equipment': 'Hanbo, Tonfa, Tanbo, Sansetsuko',
-    'attacks': (("kicks", '20'), ("punches", '10'), ("Hanbo", '45'), ("Tonfa", '45'),
-                ("Tanbo", '50'), ("Sanetsuko", '50')),
+    'attacks': [
+                (('kicks', 20), ('punches', 10), ('Hanbo', 45), ('Tonfa', 45),
+                ('Tanbo', 50), ('Sanetsuko', 50))],
 
     'Coins': {
         'Copper': 300,
@@ -34,8 +35,9 @@ Enemy_two = {
     'Level': 2,
     'Health': 100,
     'Equipment': 'Kama, Eku, Tekko, Nunti Bo',
-    'attacks': (("kicks", '20') ("punches", '10'), ("Kama", '45'),
-                ("Eku", '45'), ("Tekko", '45'), ("Nunti Bo", '45')),
+    'attacks': [
+                (('kicks', 20), ('punches', 10), ('Kama', 45),
+                ('Eku', 45), ('Tekko', 45), ('Nunti Bo', 45))],
 
     'Coins': {
         'Copper': 300,
@@ -48,8 +50,9 @@ Enemy_three = {
     'Name': 'Foot Clan',
     'Level': 3,
     'Equipment': 'Ninja Stars, Katana, Smoke Grenades',
-    'attacks': (("kicks", '20'), ("punches", '10'), ("Ninja Stars", '45'), 
-                ("Katana", '45'), ("Smoke Grenades", '45')),
+    'attacks': [
+                (('kicks', 20), ('punches', 10), ('Ninja Stars', 45), 
+                ('Katana', 45), ('Smoke Grenades', 45))],
 
     'Coins': {
         'Copper': 300,
---
+++ game.py
@@ -0,0 +1,7 @@
+from Hero_Enemy import Hero, Enemy_one, Enemy_two, Enemy_three
+
+
+# TODO: Hero attack Enemy One
+# TODO: Enemy One attack Hero
+# TODO: Check both of their health properties
+
