1376f0712f3a371cd4b984dcdf618039236645f5
dmcbdh31 <dmcbdh@gmail.com>
2022-11-04 14:44:48 -0400

    Update Hero_Enemy.py

--- Hero_Enemy.py
+++ Hero_Enemy.py
@@ -1,19 +1,11 @@
-#from asyncio import start_server
-#from decimal import ConversionSyntax
-#from email.headerregistry import ContentDispositionHeader
-#from opcode import stack_effect
-#from sys import builtin_module_names
-#from xmlrpc.client import Boolean
-
-
-
 Hero = {
     'name': 'Teenage Mutant Ninja Turtles',
     'level': 1,
     'health': 100,
     'equipment': 'Nunchucks, Sai, Katana, Bo Staff',
-    'attacks': [
-        (('power kicks, 200), (power punches, 100), ([equipment], 500)')),
+    'attacks': 
+        (("power kicks", '25'), ("power punches", '15'), ("Nunchucks", '50'), ("Sai", '50')
+        ("Katana", '50'), ("Bo Staff", '50')),
 
     'Coins': {
         'Copper': 300,
@@ -22,14 +14,13 @@ Hero = {
             }
 }       
 
-Enemy_one = [
-{
+Enemy_one = {
     'Name': 'Bebop',
     'Level': 1,
     'Health': 100,
     'Equipment': 'Hanbo, Tonfa, Tanbo, Sansetsuko',
-    'attacks': (('kicks, 20'), ('punches, 10'), ('Hanbo', 50'), ('Tonfa, 50'),
-                ('Tanbo, 50'), ('Sanetsuko, 50'))),
+    'attacks': (("kicks", '20'), ("punches", '10'), ("Hanbo", '45'), ("Tonfa", '45'),
+                ("Tanbo", '50'), ("Sanetsuko", '50')),
 
     'Coins': {
         'Copper': 300,
@@ -37,16 +28,14 @@ Enemy_one = [
         'Gold': 100
             }
 }
-]
 
-Enemy_two = [
-{
+Enemy_two = {
     'Name': 'Rocksteady',
     'Level': 2,
     'Health': 100,
     'Equipment': 'Kama, Eku, Tekko, Nunti Bo',
-    'attacks': (('kicks, 20') ('punches, 10'), ('Kama, 50'),
-                ('Eku, 50'), ('Tekko, 50'), ('Nunti Bo, 50'))),
+    'attacks': (("kicks", '20') ("punches", '10'), ("Kama", '45'),
+                ("Eku", '45'), ("Tekko", '45'), ("Nunti Bo", '45')),
 
     'Coins': {
         'Copper': 300,
@@ -54,15 +43,13 @@ Enemy_two = [
         'Gold': 100
             }
 }
-]
 
-Enemy_three = [
-{
+Enemy_three = {
     'Name': 'Foot Clan',
     'Level': 3,
     'Equipment': 'Ninja Stars, Katana, Smoke Grenades',
-    'attacks': (('kicks, 20'), ('punches, 10'), ('Ninja Stars, 50'), 
-                ('Katana, 50'), ('Smoke Grenades, 50'))),
+    'attacks': (("kicks", '20'), ("punches", '10'), ("Ninja Stars", '45'), 
+                ("Katana", '45'), ("Smoke Grenades", '45')),
 
     'Coins': {
         'Copper': 300,
@@ -70,19 +57,8 @@ Enemy_three = [
         'Gold': 100,
             } 
 }
-]
-
-enemy_list = {
-    "Enemy_one" : Enemy_one,
-    "Enemy_two" : Enemy_two,
-    "Enemy_three" : Enemy_three
-}
 
 
-import random
-enemy_attacks = "enemy_one[attacks]", "enemy_two[attacks]", "enemy_three[attacks]"
 
-rand_int = random.randrange(0-5)
-print(enemy_one[attacks]})
 
 
