3726c5fb6af2e436f9e73677e2ba72eb85bec137
dmcbdh31 <dmcbdh@gmail.com>
2022-11-04 15:46:15 -0400

    Update 4 for Hero_Enemy

--- Hero_Enemy.py
+++ Hero_Enemy.py
@@ -3,9 +3,8 @@ Hero = {
     'level': 1,
     'health': 100,
     'equipment': 'Nunchucks, Sai, Katana, Bo Staff',
-    'attacks': [ 
-            (('power kicks', 25), ('power punches', 15), ('Nunchucks', 50), 
-        ('Sai', 50), ('Katana', 50), ('Bo Staff', 50))],
+    'attacks': (("power kicks", 25), ("power punches", 15), ("Nunchucks", 50), 
+        ("Sai", 50), ("Katana", 50), ("Bo Staff", 50)),
 
     'Coins': {
         'Copper': 300,
@@ -19,9 +18,8 @@ Enemy_one = {
     'Level': 1,
     'Health': 100,
     'Equipment': 'Hanbo, Tonfa, Tanbo, Sansetsuko',
-    'attacks': [
-                (('kicks', 20), ('punches', 10), ('Hanbo', 45), ('Tonfa', 45),
-                ('Tanbo', 50), ('Sanetsuko', 50))],
+    'attacks': (("kicks", 20), ("punches", 10), ("Hanbo", 45), ("Tonfa", 45),
+                ("Tanbo", 50), ("Sanetsuko", 50)),
 
     'Coins': {
         'Copper': 300,
@@ -35,9 +33,8 @@ Enemy_two = {
     'Level': 2,
     'Health': 100,
     'Equipment': 'Kama, Eku, Tekko, Nunti Bo',
-    'attacks': [
-                (('kicks', 20), ('punches', 10), ('Kama', 45),
-                ('Eku', 45), ('Tekko', 45), ('Nunti Bo', 45))],
+    'attacks': (("kicks", 20), ("punches", 10), ("Kama", 45),
+                ("Eku", 45), ("Tekko", 45), ("Nunti Bo", 45)),
 
     'Coins': {
         'Copper': 300,
@@ -51,9 +48,8 @@ Enemy_three = {
     'Level': 3,
     'Health': 100,
     'Equipment': 'Ninja Stars, Katana, Smoke Grenades',
-    'attacks': [
-                (('kicks', 20), ('punches', 10), ('Ninja Stars', 45), 
-                ('Katana', 45), ('Smoke Grenades', 45))],
+    'attacks': (("kicks", 20), ("punches", 10), ("Ninja Stars", 45), 
+                ("Katana", 45), ("Smoke Grenades", 45)),
 
     'Coins': {
         'Copper': 300,
