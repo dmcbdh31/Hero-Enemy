2f992e502b48d36a4699b19aae6e26583ebb6a45
dmcbdh31 <dmcbdh@gmail.com>
2022-11-10 10:06:09 -0500

    Completed project

--- Hero_Enemy.py
+++ Hero_Enemy.py
@@ -2,9 +2,11 @@ Hero = {
     'name': 'Teenage Mutant Ninja Turtles',
     'level': 1,
     'health': 100,
+    'health2': 200,
+    'health3': 300,
     'equipment': 'Nunchucks, Sai, Katana, Bo Staff',
-    'attacks': (("power kicks", 25), ("power punches", 15), ("Nunchucks", 50), 
-        ("Sai", 50), ("Katana", 50), ("Bo Staff", 50)),
+    'attacks': (("power kicks", 15), ("power punches", 15), ("Nunchucks", 25), 
+        ("Sai", 25), ("Katana", 25), ("Bo Staff", 25)),
 
     'Coins': {
         'Copper': 300,
@@ -18,8 +20,8 @@ Enemy_one = {
     'Level': 1,
     'Health': 100,
     'Equipment': 'Hanbo, Tonfa, Tanbo, Sansetsuko',
-    'attacks': (("kicks", 20), ("punches", 10), ("Hanbo", 45), ("Tonfa", 45),
-                ("Tanbo", 50), ("Sanetsuko", 50)),
+    'attacks': (("kicks", 15), ("punches", 15), ("Hanbo", 20), ("Tonfa", 20),
+                ("Tanbo", 20), ("Sanetsuko", 20)),
 
     'Coins': {
         'Copper': 300,
@@ -33,8 +35,8 @@ Enemy_two = {
     'Level': 2,
     'Health': 100,
     'Equipment': 'Kama, Eku, Tekko, Nunti Bo',
-    'attacks': (("kicks", 20), ("punches", 10), ("Kama", 45),
-                ("Eku", 45), ("Tekko", 45), ("Nunti Bo", 45)),
+    'attacks': (("kicks", 15), ("punches", 15), ("Kama", 20),
+                ("Eku", 20), ("Tekko", 20), ("Nunti Bo", 20)),
 
     'Coins': {
         'Copper': 300,
@@ -48,9 +50,8 @@ Enemy_three = {
     'Level': 3,
     'Health': 100,
     'Equipment': 'Ninja Stars, Katana, Smoke Grenades',
-    'attacks': (("kicks", 20), ("punches", 10), ("Ninja Stars", 45), 
-                ("Katana", 45), ("Smoke Grenades", 45)),
-
+    'attacks': (("kicks", 15), ("punches", 15), ("Ninja Stars", 20), 
+                ("Katana", 20), ("Smoke Grenades", 20)),
     'Coins': {
         'Copper': 300,
         'Silver': 200,
--- game.py
+++ game.py
@@ -1,8 +1,108 @@
+# this is mentally exhausting for me and not fun
+# the math is not adding up right...
+
 from Hero_Enemy import Hero, Enemy_one, Enemy_two, Enemy_three
+import random
 
 
-# TODO: Hero attack Enemy One
-# TODO: Enemy One attack Hero
-# TODO: Check both of their health properties
+while Hero['health'] > 0 and Enemy_one['Health'] > 0:
 
+    random_attacks = random.choice(Hero['attacks'])
+    print(Hero['name'], "strike with", random_attacks[0])  
+    Enemy_one['Health'] -= random_attacks[1]
+    print(Enemy_one['Name'], "has",Enemy_one['Health'], "health left.")
 
+    print(" ")
+
+    random_attacks2 = random.choice(Enemy_one['attacks'])
+    print(Enemy_one['Name'], "strikes with", random_attacks2[0])
+    Hero['health'] -= random_attacks2[1]
+    print(Hero['name'], "has",Hero['health'], "health left.")    
+
+    print(" ")
+
+    if Enemy_one['Health'] > 0:
+
+        print(Enemy_one['Name'], "strikes with", random_attacks2[0])
+    Hero['health'] -= random_attacks2[1]
+    print(Hero['name'], "has",Hero['health'], "health left.")
+
+    if Hero['health'] > 0:
+
+        print(Hero['name'], "strikes with", random_attacks[0])
+    Enemy_one['Health'] -= random_attacks[1]
+    print(Enemy_one['Name'], "has",Enemy_one['Health'], "health left.")
+         
+    if Hero['health'] > Enemy_one['Health']:
+        print(f"{Hero['name']} defeated {Enemy_one['Name']}. You are moving up to Level 2.")
+        break
+    else:
+        print(f"{Enemy_one['Name']} defeated {Hero['name']}. Try again.") 
+
+while Hero['health2'] > 0 and Enemy_two['Health'] > 0:
+
+    random_attacks = random.choice(Hero['attacks'])
+    print(Hero['name'], "strike with", random_attacks[0])  
+    Enemy_two['Health'] -= random_attacks[1]
+    print(Enemy_two['Name'], "has",Enemy_two['Health'], "health left.")
+
+    print(" ")
+
+    random_attacks3 = random.choice(Enemy_two['attacks'])
+    print(Enemy_two['Name'], "strikes with", random_attacks3[0])
+    Hero['health2'] -= random_attacks3[1]
+    print(Hero['name'], "has",Hero['health2'], "health left.")    
+
+    print(" ")
+
+    if Enemy_two['Health'] > 0:
+
+        print(Enemy_two['Name'], "strikes with", random_attacks3[0])
+    Hero['health'] -= random_attacks3[1]
+    print(Hero['name'], "has",Hero['health'], "health left.")
+
+    if Hero['health'] > 0:
+
+        print(Hero['name'], "strikes with", random_attacks[0])
+    Enemy_two['Health'] -= random_attacks[1]
+    print(Enemy_two['Name'], "has",Enemy_two['Health'], "health left.")
+         
+    if Hero['health2'] > Enemy_one['Health']:
+        print(f"{Hero['name']} defeated {Enemy_two['Name']}. You are moving up to Level 3.")
+        break
+    else:
+        print(f"{Enemy_two['Name']} defeated {Hero['name']}. Try again.")
+
+while Hero['health3'] > 0 and Enemy_three['Health'] > 0:
+
+    random_attacks = random.choice(Hero['attacks'])
+    print(Hero['name'], "strike with", random_attacks[0])  
+    Enemy_three['Health'] -= random_attacks[1]
+    print(Enemy_three['Name'], "has",Enemy_three['Health'], "health left.")
+
+    print(" ")
+
+    random_attacks4 = random.choice(Enemy_three['attacks'])
+    print(Enemy_three['Name'], "strikes with", random_attacks4[0])
+    Hero['health3'] -= random_attacks4[1]
+    print(Hero['name'], "has",Hero['health3'], "health left.")    
+
+    print(" ")
+
+    if Enemy_three['Health'] > 0:
+
+        print(Enemy_three['Name'], "strikes with", random_attacks3[0])
+    Hero['health'] -= random_attacks3[1]
+    print(Hero['name'], "has",Hero['health'], "health left.")
+
+    if Hero['health'] > 0:
+
+        print(Hero['name'], "strikes with", random_attacks[0])
+    Enemy_three['Health'] -= random_attacks[1]
+    print(Enemy_three['Name'], "has",Enemy_three['Health'], "health left.")
+         
+    if Hero['health3'] > Enemy_one['Health']:
+        print(f"{Hero['name']} defeated {Enemy_three['Name']}. You win.")
+        break
+    else:
+        print(f"{Enemy_three['Name']} defeated {Hero['name']}. Try again.")
